1b1aabf309a5a5a2f28f0a8769771f30f671d750
Steven Thonus <steven.thonus@infi.nl>
2019-04-03 15:35:58 +0200

    use absolute path on manifest

--- src-server/bamse/render.cljs
+++ src-server/bamse/render.cljs
@@ -55,7 +55,7 @@
               :href "/css/main.css"}]
 
       [:link {:rel "manifest"
-              :href "manifest.json"}]
+              :href "/manifest.json"}]
       [:link {:rel   "apple-touch-icon"
               :sizes "180x180"
               :href  "/apple-touch-icon.png"}]
@@ -67,8 +67,6 @@
               :type  "image/png"
               :sizes "16x16"
               :href  "/favicon-16x16.png"}]
-      [:link {:rel  "manifest"
-              :href "/site.webmanifest"}]
       [:link {:rel   "mask-icon"
               :href  "/safari-pinned-tab.svg"
               :color "#3f6f7d"}]
